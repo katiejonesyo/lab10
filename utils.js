44018c7595e53253c369e4d63aef70260f08e11c
Katelinn Jones <katiejonesyo@gmail.com>
2020-11-10 12:04:29 -0800

    trails info done but its not showing up?

--- utils.js
+++ utils.js
@@ -15,6 +15,26 @@ function mungeWeather(location) {
     }).slice(0,8);
 }
 
+
+
+function mungeTrail(trail) {
+
+    return trail.trails.map(item => {
+        return {
+            name: item.name,
+            location: item.location,
+            length: item.length,
+            stars: item.stars,
+            star_votes:item.starVotes,
+            summary: item.summary,
+            trail_url: item.url,
+            conditions: item.conditionStatus,
+            condition_date: item.conditionDate.split('')[0],
+            condition_time: item.conditionTime.split('')[1],
+         };
+    }).slice(0, 10);
+}
+
 module.exports = {
-    mungeLocation, mungeWeather
+    mungeLocation, mungeWeather, mungeTrail
 };
